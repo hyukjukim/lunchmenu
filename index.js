42e39756a91312327faa008aecf4e16fa2fa69b5
hyukjukim <hyukjukim@koreanair.com>
2016-12-05 17:32:56 +0900

    20161205

--- index.js
+++ index.js
@@ -21,6 +21,12 @@ app.get('/', function (req, res) {
     res.send('Hello world, I am a chat bot')
 })
 
+// Spin up the server
+app.listen(app.get('port'), function() {
+    console.log('running on port', app.get('port'))
+})
+
+
 // 카카오톡 연결 1
 app.get('/keyboard', function (req, res) {
     res.send({
