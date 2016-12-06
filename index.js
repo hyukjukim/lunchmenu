8d38721ca5d3ad7bef3c3da7e1ec9809a713156b
hyukjukim <hyukjukim@koreanair.com>
2016-12-06 18:29:35 +0900

    20161206

--- index.js
+++ index.js
@@ -70,15 +70,16 @@ app.post('/message', function(req, res) {
         "text" : "테스트 페이지 입니다!"
     }
 }
+
+);
+
 res.send(    if(user_key == 'O2x5vV9_6vB5'){
       res.send('오 김혁주님 안녕하세요.')
     }
     if(user_key !== 'O2x5vV9_6vB5'){
       res.send('당신은 누구십니까?')
     })
-);
-
-
+    
     console.log(req.body);
     Contact.create({
         user_key : req.body.user_key,
