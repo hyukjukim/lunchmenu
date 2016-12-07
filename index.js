a5eb1bbc2ee797439182fad5777879071f1d811c
hyukjukim <hyukjukim@koreanair.com>
2016-12-07 15:37:02 +0900

    20161207

--- index.js
+++ index.js
@@ -10,38 +10,81 @@
 
 var express = require('express');
 var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
 var app = express();
+
 
 //DB Setting
 mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
-
+//Connection Succeed
 db.once("open", function(){
   console.log("****************** MONGO_DB CONNECTED ******************");
 });
 
+//Connection Failed
 db.on('error', function(err){
   console.log("****************** DB CONNECTION ERR : ******************" ,err);
 });
-
 
 //Other setttings
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
 
-// 2
+// bodyParser로 stream의 form data를 req.body에 옮겨 담습니다. 2번은 json data를, 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+//mongoose.Schema 함수를 사용해서 schema(data구조를 미리 정의해 놓는 것) object를 생성합니다
+var contactSchema = mongoose.Schema({
+ name:{type:String, required:true, unique:true},
+ email:{type:String},
+ phone:{type:String}
+});
+
+//mongoose.model함수를 사용하여 contact schema의 model을 생성합니다
+var Contact = mongoose.model("contact", contactSchema); //5
+
+app.get("/",function(req, res){
+  res.redirect("/contacts");
+});
+
+//INDEX
+app.get("/contacts",function(req, res){
+  Contact.find({},function(err,contacts){
+    if(err) return res.json(err);
+    res.resnder("contacts/index", {contacts:contacts});
+  });
+});
+
+// NEW
+app.get("/contacts/new", function(req, res){
+ res.render("contacts/new");
+});
+
+// CREATE
+app.post("/contacts", function(req, res){
+ Contact.create(req.body, function(err, contact){
+  if(err) return res.json(err);
+  res.redirect("/contacts");
+ });
+});
+
+
+/******************************************************************************
+
+//{nameQuery:'value'}를 뽑아주는 구문. 주소 끝에 ?nameQuery='value'써줘야 한다.
 app.get("/hello", function(req,res){
- res.render("hello", {name:req.query.nameQuery}); //{nameQuery:'value'}를 뽑아주는 구문. 주소 끝에 ?nameQuery='value'써줘야 한다.
+ res.render("hello", {name:req.query.nameQuery});
 console.log(req.query);
 });
 
-// 3
+//파라미터에 입력하는 값을 name value로 넣는 구문
 app.get("/hello/:nameParam", function(req,res){
- res.render("hello", {name:req.params.nameParam}); //파라미터에 입력하는 값을 name value로 넣는 구문
+ res.render("hello", {name:req.params.nameParam});
  console.log(req.query);
 });
-
-
+******************************************************************************/
 
 //Port Setting
 app.listen(3000, function(){
