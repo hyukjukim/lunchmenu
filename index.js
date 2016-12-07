12703209904a97447d998132a71a914283c7d8d8
hyukjukim <hyukjukim@koreanair.com>
2016-12-07 17:43:45 +0900

    20161207

--- index.js
+++ index.js
@@ -11,6 +11,7 @@
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require("body-parser");
+var methodOverride = require("method-override"); // 1
 var app = express();
 
 
@@ -27,14 +28,6 @@ db.on('error', function(err){
   console.log("****************** DB CONNECTION ERR : ******************" ,err);
 });
 
-//Other setttings
-app.set("view engine", "ejs");
-app.use(express.static(__dirname+"/public"));
-
-// bodyParser로 stream의 form data를 req.body에 옮겨 담습니다. 2번은 json data를, 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
-
 //mongoose.Schema 함수를 사용해서 schema(data구조를 미리 정의해 놓는 것) object를 생성합니다
 var contactSchema = mongoose.Schema({
  name:{type:String, required:true, unique:true},
@@ -45,26 +38,90 @@ var contactSchema = mongoose.Schema({
 //mongoose.model함수를 사용하여 contact schema의 model을 생성합니다
 var Contact = mongoose.model("contact", contactSchema); //5
 
-app.get("/",function(req, res){
-  res.redirect("/contacts");
+//Other setttings
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/"));
+
+// bodyParser로 stream의 form data를 req.body에 옮겨 담습니다. 2번은 json data를, 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method")); // 2
+
+
+
+app.get("/", function(req, res){
+ res.redirect("/contacts");
 });
 
 //INDEX
 app.get("/contacts",function(req, res){
   Contact.find({},function(err,contacts){
     if(err) return res.json(err);
-    res.resnder("contacts/index", {contacts:contacts});
+    res.render("contacts/index", {contacts:contacts});
+    console.log(req.body);
+   console.log('********************다음**************');
+   console.log(req.query);
+   console.log('********************다음**************');
+   console.log(req.cookies);
+   console.log('********************다음**************');
+   console.log(req.url);
   });
 });
 
 // NEW
 app.get("/contacts/new", function(req, res){
  res.render("contacts/new");
+ console.log(req.body);
+console.log('********************다음**************');
+console.log(req.query);
+console.log('********************다음**************');
+console.log(req.cookies);
+console.log('********************다음**************');
+console.log(req.url);
 });
 
 // CREATE
 app.post("/contacts", function(req, res){
  Contact.create(req.body, function(err, contact){
+  if(err) return res.json(err);
+  res.redirect("/contacts");
+  console.log(req.body);
+ console.log('********************다음**************');
+ console.log(req.query);
+ console.log('********************다음**************');
+ console.log(req.cookies);
+ console.log('********************다음**************');
+ console.log(req.url);
+ });
+});
+
+// Contacts - show // 3
+app.get("/contacts/:id", function(req, res){
+ Contact.findOne({_id:req.params.id}, function(err, contact){
+  if(err) return res.json(err);
+  res.render("contacts/show", {contact:contact});
+ });
+});
+
+// Contacts - edit // 4
+app.get("/contacts/:id/edit", function(req, res){
+ Contact.findOne({_id:req.params.id}, function(err, contact){
+  if(err) return res.json(err);
+  res.render("contacts/edit", {contact:contact});
+ });
+});
+
+// Contacts - update // 5
+app.put("/contacts/:id", function(req, res){
+ Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+  if(err) return res.json(err);
+  res.redirect("/contacts/"+req.params.id);
+ });
+});
+
+// Contacts - destroy // 6
+app.delete("/contacts/:id", function(req, res){
+ Contact.remove({_id:req.params.id}, function(err, contact){
   if(err) return res.json(err);
   res.redirect("/contacts");
  });
