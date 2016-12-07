c3f66adfa672f7902bb2d752018130ae40dd01c0
hyukjukim <hyukjukim@koreanair.com>
2016-12-07 17:48:05 +0900

    20161207

--- index.js
+++ index.js
@@ -1,157 +1,149 @@
-'use strict'
+/* 기본 로그 보는 구문
+  console.log(req.body);
+ console.log('********************다음**************');
+ console.log(req.query);
+ console.log('********************다음**************');
+ console.log(req.cookies);
+ console.log('********************다음**************');
+ console.log(req.url);
+ */
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
-const app = express()
-var mongoose = require("mongoose")
-const token = process.env.FB_TOKEN // 환경변수 갖고 오는 곳.
+var express = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override"); // 1
+var app = express();
 
 
-// DB setting
-mongoose.connect(process.env.MONGO_DB); // 1
-var db = mongoose.connection; // 2
-// 3﻿
+//DB Setting
+mongoose.connect(process.env.MONGO_DB);
+var db = mongoose.connection;
+//Connection Succeed
 db.once("open", function(){
- console.log("DB connected");
+  console.log("****************** MONGO_DB CONNECTED ******************");
 });
-// 4
-db.on("error", function(err){
- console.log("DB ERROR : ", err);
+
+//Connection Failed
+db.on('error', function(err){
+  console.log("****************** DB CONNECTION ERR : ******************" ,err);
 });
-// DB schema // 4
+
+//mongoose.Schema 함수를 사용해서 schema(data구조를 미리 정의해 놓는 것) object를 생성합니다
 var contactSchema = mongoose.Schema({
-    user_key: {
-        type: String
-    }, //name:{type:String, required:true, unique:true},
-    type: {
-        type: String
-    },
-    content: {
-        type: String
-    }
+ name:{type:String, required:true, unique:true},
+ email:{type:String},
+ phone:{type:String}
 });
+
+//mongoose.model함수를 사용하여 contact schema의 model을 생성합니다
 var Contact = mongoose.model("contact", contactSchema); //5
 
+//Other setttings
+app.set("view engine", "ejs");
+app.use(express.static(__dirname+"/"));
 
-app.set('port', (process.env.PORT || 5000))
-
-// Process application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: false}))
-
-// Process application/json
-app.use(bodyParser.json())
-
-// Index route
-app.get('/', function (req, res) {
-    res.send('Hello world, I am a FaceBook chat bot')
-})
-
-// for Facebook verification
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === 'my_voice_is_my_password_verify_me') {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send('Error, wrong token')
-})
-
-// Spin up the server
-app.listen(app.get('port'), function() {
-    console.log('running on port', app.get('port'))
-})
+// bodyParser로 stream의 form data를 req.body에 옮겨 담습니다. 2번은 json data를, 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method")); // 2
 
 
-app.post('/webhook/', function (req, res) {
-  let messaging_events = req.body.entry[0].messaging
-  for (let i = 0; i < messaging_events.length; i++) {
-    let event = req.body.entry[0].messaging[i]
-    let sender = event.sender.id
-    if (event.message && event.message.text) {
-      let text = event.message.text
-      if (text === 'Generic') {
-          sendGenericMessage(sender)
-          continue
-      }
-      sendTextMessage(sender, "Text received, echo: " + text.substring(0, 200))
-      Contact.create({ content:  text.substring(0, 200) }, function(error, doc) {
-  // doc.children[0]._id will be undefined
+
+app.get("/", function(req, res){
+ res.redirect("/contacts");
 });
-    }
-    if (event.postback) {
-      let text = JSON.stringify(event.postback)
-      sendTextMessage(sender, "Postback received: "+text.substring(0, 200), token)
-      continue
-    }
-  }
-  res.sendStatus(200)
-})
+
+//INDEX
+app.get("/contacts",function(req, res){
+  Contact.find({},function(err,contacts){
+    if(err) return res.json(err);
+    res.render("contacts/index", {contacts:contacts});
+    console.log(req.body);
+   console.log('********************다음**************');
+   console.log(req.query);
+   console.log('********************다음**************');
+   console.log(req.cookies);
+   console.log('********************다음**************');
+   console.log(req.url);
+  });
+});
+
+// NEW
+app.get("/contacts/new", function(req, res){
+ res.render("contacts/new");
+ console.log(req.body);
+console.log('********************다음**************');
+console.log(req.query);
+console.log('********************다음**************');
+console.log(req.cookies);
+console.log('********************다음**************');
+console.log(req.url);
+});
+
+// CREATE
+app.post("/contacts", function(req, res){
+ Contact.create(req.body, function(err, contact){
+  if(err) return res.json(err);
+  res.redirect("/contacts");
+  console.log(req.body);
+ console.log('********************다음**************');
+ console.log(req.query);
+ console.log('********************다음**************');
+ console.log(req.cookies);
+ console.log('********************다음**************');
+ console.log(req.url);
+ });
+});
+
+// Contacts - show // 3
+app.get("/contacts/:id", function(req, res){
+ Contact.findOne({_id:req.params.id}, function(err, contact){
+  if(err) return res.json(err);
+  res.render("contacts/show", {contact:contact});
+ });
+});
+
+// Contacts - edit // 4
+app.get("/contacts/:id/edit", function(req, res){
+ Contact.findOne({_id:req.params.id}, function(err, contact){
+  if(err) return res.json(err);
+  res.render("contacts/edit", {contact:contact});
+ });
+});
+
+// Contacts - update // 5
+app.put("/contacts/:id", function(req, res){
+ Contact.findOneAndUpdate({_id:req.params.id}, req.body, function(err, contact){
+  if(err) return res.json(err);
+  res.redirect("/contacts/"+req.params.id);
+ });
+});
+
+// Contacts - destroy // 6
+app.delete("/contacts/:id", function(req, res){
+ Contact.remove({_id:req.params.id}, function(err, contact){
+  if(err) return res.json(err);
+  res.redirect("/contacts");
+ });
+});
 
 
-function sendTextMessage(sender, text) {
-    let messageData = { text:text }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
+/******************************************************************************
 
+//{nameQuery:'value'}를 뽑아주는 구문. 주소 끝에 ?nameQuery='value'써줘야 한다.
+app.get("/hello", function(req,res){
+ res.render("hello", {name:req.query.nameQuery});
+console.log(req.query);
+});
 
-function sendGenericMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "generic",
-                "elements": [{
-                    "title": "First card",
-                    "subtitle": "Element #1 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
-                    "buttons": [{
-                        "type": "web_url",
-                        "url": "https://www.messenger.com",
-                        "title": "web url"
-                    }, {
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for first element in a generic bubble",
-                    }],
-                }, {
-                    "title": "Second card",
-                    "subtitle": "Element #2 of an hscroll",
-                    "image_url": "http://messengerdemo.parseapp.com/img/gearvr.png",
-                    "buttons": [{
-                        "type": "postback",
-                        "title": "Postback",
-                        "payload": "Payload for second element in a generic bubble",
-                    }],
-                }]
-            }
-        }
-    }
-    request({
-        url: 'https://graph.facebook.com/v2.6/me/messages',
-        qs: {access_token:token},
-        method: 'POST',
-        json: {
-            recipient: {id:sender},
-            message: messageData,
-        }
-    }, function(error, response, body) {
-        if (error) {
-            console.log('Error sending messages: ', error)
-        } else if (response.body.error) {
-            console.log('Error: ', response.body.error)
-        }
-    })
-}
+//파라미터에 입력하는 값을 name value로 넣는 구문
+app.get("/hello/:nameParam", function(req,res){
+ res.render("hello", {name:req.params.nameParam});
+ console.log(req.query);
+});
+******************************************************************************/
+
+//Port Setting
+app.listen(3000, function(){
+  console.log("Server on!");
+});
