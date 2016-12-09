5c669c12674ce23eda5dbd5a19218516ab860312
hyukjukim <hyukjukim@koreanair.com>
2016-12-09 17:12:41 +0900

    20161209

--- models/post.js
+++ models/post.js
@@ -12,7 +12,7 @@ var postSchema = mongoose.Schema({ // 1
  toObject:{virtuals:true} // 4
 });
 
-// virtuals // 3
+// virtuals : virtuals은 실제 DB에 저장되진 않지만 model에서는 db에 있는 다른 항목들과 동일하게 사용할 수 있음
 postSchema.virtual("createdDate")
 .get(function(){
  return getDate(this.createdAt);
