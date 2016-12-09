52b71d010f8000641fbbf7453fa9899f27af5ad6
hyukjukim <hyukjukim@koreanair.com>
2016-12-09 15:53:29 +0900

    20161209

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ mongoose.connect(process.env.MONGO_DB);
 var db = mongoose.connection;
 //Connection Succeed
 db.once("open", function(){
-  console.log("****************** MONGO_DB CONNECTED ******************");
+  console.log("MONGO_DB CONNECTED");
 });
 //Connection Failed
 db.on('error', function(err){
-  console.log("****************** DB CONNECTION ERR : ******************" ,err);
+  console.log("DB CONNECTION ERR : " ,err);
 });
 
-
-app.set('port', (process.env.PORT || 5000));
+app.set('port', (process.env.PORT||5000));
 
 //Other setttings
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/"));
-// bodyParser로 stream의 form data를 req.body에 옮겨 담습니다. 2번은 json data를, 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
+// bodyParser로 stream의 form data를 req.body에 옮겨 담습니다.
+// 2번은 json data를, 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
@@ -36,8 +36,12 @@ app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
 });
 
-/******************************************************************************
 
+
+
+
+
+/******************************************************************************
 //{nameQuery:'value'}를 뽑아주는 구문. 주소 끝에 ?nameQuery='value'써줘야 한다.
 app.get("/hello", function(req,res){
  res.render("hello", {name:req.query.nameQuery});
