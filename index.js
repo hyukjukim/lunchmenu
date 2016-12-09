9906ddd252c231d48c3953be546a7e25ae0ae5b8
hyukjukim <hyukjukim@koreanair.com>
2016-12-09 17:37:29 +0900

    20161209

--- index.js
+++ index.js
@@ -27,8 +27,8 @@ app.use(methodOverride("_method"));
 // Routes
 app.use("/", require("./routes/home"));
 app.use("/posts", require("./routes/posts"));
-app.use("/contacts", require("./routes/contacts"));
-app.use("/kakao", require("./routes/kakao"));
+//app.use("/contacts", require("./routes/contacts"));
+//app.use("/kakao", require("./routes/kakao"));
 
 // Port setting
 app.listen(app.get('port'), function() {
