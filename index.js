e4ffb93139ebe2d367b92e17cab3f4f9b5f47d20
hyukjukim <hyukjukim@koreanair.com>
2016-12-09 16:30:46 +0900

    20161209

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ db.on("error", function(err){
 // Other settings
 app.set('port', (process.env.PORT||5000));
 app.set("view engine", "ejs");
-app.use(express.static(__dirname+"/public"));
+app.use(express.static(__dirname+"/"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
--- routes/home.js
+++ routes/home.js
@@ -3,9 +3,12 @@
 var express = require("express");
 var router = express.Router();
 
-// home
+// Home
 router.get("/", function(req, res){
-  res.redirect("/contacts");
+ res.render("home/welcome");
+});
+router.get("/about", function(req, res){
+ res.render("home/about");
 });
 
 module.exports = router;
