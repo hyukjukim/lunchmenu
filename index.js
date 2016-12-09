bbbde5d6721dccb43adaefe5812a8a3c0008d8d1
hyukjukim <hyukjukim@koreanair.com>
2016-12-09 16:17:06 +0900

    20161209

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override"); // 1
+// index.js
+
+var express    = require("express");
+var mongoose   = require("mongoose");
+var bodyParser  = require("body-parser");
+var methodOverride = require("method-override");
 var app = express();
 
-//DB Setting
-mongoose.connect(process.env.MONGO_DB);
+// DB setting
+mongoose.connect(process.env.MONGO_DB); // 1
 var db = mongoose.connection;
-//Connection Succeed
 db.once("open", function(){
-  console.log("MONGO_DB CONNECTED");
+ console.log("DB connected");
 });
-//Connection Failed
-db.on('error', function(err){
-  console.log("DB CONNECTION ERR : " ,err);
+db.on("error", function(err){
+ console.log("DB ERROR : ", err);
 });
 
+// Other settings
 app.set('port', (process.env.PORT||5000));
-
-//Other setttings
 app.set("view engine", "ejs");
-app.use(express.static(__dirname+"/"));
-// bodyParser로 stream의 form data를 req.body에 옮겨 담습니다.
-// 2번은 json data를, 3번은 urlencoded data를 분석해서 req.body를 생성합니다.
+app.use(express.static(__dirname+"/public"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 
 // Routes
 app.use("/", require("./routes/home"));
-//app.use("/kakao", require("./routes/kakao"));
 
-// Spin up the server
+// Port setting
 app.listen(app.get('port'), function() {
     console.log('running on port', app.get('port'));
 });
+
+
 
 
 
