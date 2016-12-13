18d7eb63b8120b031e254fec01cd2e3c49377969
hyukjukim <hyukjukim@koreanair.com>
2016-12-13 14:25:19 +0900

    20161213

--- routes/kakao.js
+++ routes/kakao.js
@@ -2,7 +2,7 @@
 var express = require("express");
 var router = express.Router();
 var KakaoMsg = require("../models/KakaoMsg");
-var KakaoUser  = require("../models/KakaoUser"); //유저 ID 매칭을 위한 작업
+var KakaoUser = require("../models/KakaoUser"); //유저 ID 매칭을 위한 작업
 
 // 카카오톡 연결 1
 router.get('/keyboard', function(req, res) {
@@ -14,66 +14,62 @@ router.get('/keyboard', function(req, res) {
 
 router.post('/message', function(req, res) {
 
+    //상대가 처음 말을 걸었을 경우 동작. 상대의 USER KEY 값과 FLAG 정보를 입력한다.
+    KakaoUser.create({
+        user_key: req.body.user_key,
+        name_flag: '0',
+        password_flag: '0',
+        email_flag: '0'
+    }, function(error, doc) {});
 
-  KakaoUser.create({
-      user_key : req.body.user_key,
-      name_flag:'0',
-      password_flag:'0' ,
-      email_flag:'0'
-  }, function(error, doc) {
-  });
-
-/*
-  KakaoUser.findOne({'name':'guitar84'}, function (err, users) {
-    if (err) return res.json(err);
-    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', {users}) ;
-    console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', {users:users.name}) ;
-  });
-
-*///req.body.user_key === KakaoUser.findOne({'user_key':'guitar84'}
-//메세지를 기록하는 부분
-
-
-
-//KAKAO TALK USER_KEY를 받아와서 업데이트 하는 부분
-console.log('************************************'+ req.body.user_key);
-KakaoUser.findOneAndUpdate({'user_key': req.body.user_key},{'name_flag':'1'},{new: true}, function(err, doc){
-    if(err){
-        console.log("Something wrong when updating data!");
-    }
-});
-
-
-
-
-      KakaoUser.findOne({'name':'김혁주ㅁ'}, function (err, users) {
+  //  if ()
+    /*
+      KakaoUser.findOne({'name':'guitar84'}, function (err, users) {
         if (err) return res.json(err);
-        //console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', {users}) ;
-        //console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', {users:users.name}) ;
+        console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@', {users}) ;
+        console.log('$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$', {users:users.name}) ;
       });
 
-    if(req.body.content == '시작'){
-      res.send({
-          "message":{
-              "text" : "시작 버튼을 누르셨습니다. 아직 기능 구현 중 입니다.(2016.12.9)\n월요일에 추가 구현 할 예정 입니다."
-          }
-      });
+    */ //req.body.user_key === KakaoUser.findOne({'user_key':'guitar84'}
+    //메세지를 기록하는 부분
+
+
+    if (req.body.content == '시작') {
+        res.send({
+            "message": {
+                "text": "시작 버튼을 누르셨습니다. 아직 기능 구현 중 입니다.(2016.12.9)\n월요일에 추가 구현 할 예정 입니다."
+            }
+        });
     }
 
-    if(req.body.content === '닉네임설정'){
-      res.send({
-          "message":{
-              "text" : "사용하실 닉네임을 입력해주세요. 아직 기능 구현 중 입니다.(2016.12.9)."
-          }
-      });
+    if (req.body.content === '닉네임설정') {
+        res.send({
+            "message": {
+                "text": "사용하실 닉네임을 입력해주세요. 아직 기능 구현 중 입니다.(2016.12.9)."
+            }
+        });
+
+        //KAKAO TALK USER_KEY를 받아와서 업데이트 하는 부분
+        console.log('************************************' + req.body.user_key);
+        KakaoUser.findOneAndUpdate({
+            'user_key': req.body.user_key
+        }, {
+            'name_flag': '1', 'name' : '김혁주'
+        }, {
+            new: true
+        }, function(err, doc) {
+            if (err) {
+                console.log("Something wrong when updating data!");
+            }
+        });
     }
 
-    if(req.body.content === '내정보변경'){
-      res.send({
-          "message":{
-              "text" : "정보 변경을 입력하셨습니다. 아직 기능 구현 중 입니다.(2016.12.9)."
-          }
-      });
+    if (req.body.content === '내정보변경') {
+        res.send({
+            "message": {
+                "text": "정보 변경을 입력하셨습니다. 아직 기능 구현 중 입니다.(2016.12.9)."
+            }
+        });
     }
     //console.log(req.body);
 
@@ -89,14 +85,13 @@ KakaoUser.findOneAndUpdate({'user_key': req.body.user_key},{'name_flag':'1'},{ne
 
     //메세지를 기록하는 부분
     KakaoMsg.create({
-        user_key : req.body.user_key,
-        type    : req.body.type,
+        user_key: req.body.user_key,
+        type: req.body.type,
         content: req.body.content
-    }, function(error, doc) {
-    });
+    }, function(error, doc) {});
 
 
-   res.sendStatus(200);
+    res.sendStatus(200);
 });
 
 router.post('/friend', function(req, res) {
