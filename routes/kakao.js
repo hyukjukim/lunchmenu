36643039745ecb9fcc9317e564ca6255cfcf8054
hyukjukim <hyukjukim@koreanair.com>
2016-12-16 15:39:16 +0900

    20161216

--- routes/kakao.js
+++ routes/kakao.js
@@ -15,16 +15,36 @@ router.get('/keyboard', function(req, res) {
 
 router.post('/message', function(req, res) {
 
-    //상대가 처음 말을 걸었을 경우 동작. 상대의 USER KEY 값과 FLAG 정보를 입력한다.
-    KakaoUser.create({
-        user_key: req.body.user_key,
-        name_flag: '0',
-        password_flag: '0',
-        email_flag: '0'
-    }, function(error, doc) {});
+
 
     if (req.body.content === '닉네임설정') {
+      if(name_flag_cnt === 0){
+            res.send({
+                "message": {
+                    "text": "닉네임설정 버튼을 누르셨습니다. 닉네임을 입력해 주세요."
+                }
+            });
+            KakaoUser.findOne({'user_key':req.body.user_key}, function (err, users) {
+        if (err) return res.json(err);
+        if({users}.users.name_flag ==='0'){
+          console.log('111111111', {users}) ; //전체 객체
+          KakaoUser.findOneAndUpdate({'user_key': req.body.user_key}, {'name_flag': '1'}, {new: true}, function(err, doc) {
+              if (err) {console.log("Something wrong when updating data!");}
 
+          });
+        }
+      });
+      }
+}
+
+KakaoUser.findOne({'user_key':req.body.user_key}, function (err, users) {
+if (err) return res.json(err);
+    name_flag_cnt = {users}.users.name_flag;
+});
+
+
+    console.log(name_flag_cnt+'@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' + req.body.content);
+if(name_flag_cnt === 1){
 res.send({
     "message": {
         "text": "닉네임설정 버튼을 누르셨습니다. 닉네임을 입력해 주세요."
