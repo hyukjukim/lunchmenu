b0f38bd01214470ab22b3a5cf6113bee17b32d34
hyukjukim <hyukjukim@koreanair.com>
2016-12-16 15:09:27 +0900

    20161216

--- routes/kakao.js
+++ routes/kakao.js
@@ -30,18 +30,16 @@ router.post('/message', function(req, res) {
                     "text": "닉네임설정 버튼을 누르셨습니다. 닉네임을 입력해 주세요."
                 }
             });
-            KakaoUser.findOne({'user_key':req.body.user_key}, function (err, users) {
-        if (err) return res.json(err);
-        if({users}.users.name_flag ==='0'){
+
+  //{users}.users.name_flag ==='0')
           console.log('111111111', {users}) ; //전체 객체
           KakaoUser.findOneAndUpdate({'user_key': req.body.user_key}, {'name_flag': '1'}, {new: true}, function(err, doc) {
               if (err) {console.log("Something wrong when updating data!");}
 
           });
-        }
-      });
+
       }
-}
+    }
 
 KakaoUser.findOne({'user_key':req.body.user_key}, function (err, users) {
 if (err) return res.json(err);
