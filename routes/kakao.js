202116533bd71a424965a73edf8522825a5151de
hyukjukim <hyukjukim@koreanair.com>
2016-12-16 14:56:41 +0900

    20161216

--- routes/kakao.js
+++ routes/kakao.js
@@ -15,6 +15,46 @@ router.get('/keyboard', function(req, res) {
 
 router.post('/message', function(req, res) {
 
+    //상대가 처음 말을 걸었을 경우 동작. 상대의 USER KEY 값과 FLAG 정보를 입력한다.
+    KakaoUser.create({
+        user_key: req.body.user_key,
+        name_flag: '0',
+        password_flag: '0',
+        email_flag: '0'
+    }, function(error, doc) {});
+
+    if (req.body.content === '닉네임설정') {
+      if(name_flag_cnt === 0){
+            res.send({
+                "message": {
+                    "text": "닉네임설정 버튼을 누르셨습니다. 닉네임을 입력해 주세요."
+                }
+            });
+            KakaoUser.findOne({'user_key':req.body.user_key}, function (err, users) {
+        if (err) return res.json(err);
+        if({users}.users.name_flag ==='0'){
+          console.log('111111111', {users}) ; //전체 객체
+          KakaoUser.findOneAndUpdate({'user_key': req.body.user_key}, {'name_flag': '1'}, {new: true}, function(err, doc) {
+              if (err) {console.log("Something wrong when updating data!");}
+              name_flag_cnt = 1;
+          });
+        }
+      });
+      }
+}
+    console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' + req.body.content);
+if(name_flag_cnt === 1){
+res.send({
+    "message": {
+        "text": "닉네임설정 버튼을 누르셨습니다. 닉네임을 입력해 주세요."
+    }
+});
+    console.log('ASDFASDFASDFASDFASDF' + req.body.content);
+}
+    //req.body.user_key === KakaoUser.findOne({'user_key':'guitar84'}
+    //메세지를 기록하는 부분
+
+/*
     if (req.body.content == '시작') {
         res.send({
             "message": {
@@ -34,7 +74,18 @@ router.post('/message', function(req, res) {
             }
         });
     }
+    */
+    //console.log(req.body);
 
+    /*
+    //유저 이름을 기억하는 부분
+    User.create({
+        username : req.body.user_key, //이름은 고유값
+        name    : req.body.type, //닉네임은 아무거나
+        password : req.body.content //비밀번호 설정
+    }, function(error, doc) {
+    });
+    */
 
     //메세지를 기록하는 부분
     KakaoMsg.create({
