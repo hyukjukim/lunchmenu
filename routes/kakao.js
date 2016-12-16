bf77ebe72defa70c5c40ea15fa5db568b7f9ba2a
hyukjukim <hyukjukim@koreanair.com>
2016-12-16 16:14:21 +0900

    20161216

--- routes/kakao.js
+++ routes/kakao.js
@@ -49,16 +49,13 @@ router.post('/message', function(req, res) {
 KakaoUser.findOne({'user_key':req.body.user_key}, function (err, users) {
 if (err) return res.json(err);
 console.log("gggggggggggggggggggg"+{users}.users.name_flag);
-array_cnt.push({users}.users.name_flag);
-});
 
-if(array_cnt.pop()==='1'){
+});
   res.send({
       "message": {
           "text": "닉네임설정 버튼을 누르셨습니다. 닉네임을 입력해 주세요."
       }
   });
-}
 //유저 flag 변수 사용
 console.log('@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@' + req.body.content);
 
