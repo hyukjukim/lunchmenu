27b3274f95c139370af5aa8a8b1992fa2b0f992a
hyukjukim <hyukjukim@koreanair.com>
2016-12-28 17:50:10 +0900

    20151228

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/webhook/', function (req, res) {
 
       client.message(sender,text, {})
       .then((data) => {
-        console.log(data);
+        console.log('Yay, got Wit.ai response: '+data);
         var obj = JSON.stringify(data);
         var result = JSON.parse(obj);
         console.log('Yay, got Wit.ai response: ' /*+ JSON.stringify(result.entities.intent[0].value)*/);
