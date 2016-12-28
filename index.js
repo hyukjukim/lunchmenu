0e315e0310fb0eb2b13ee4acf2fbc01ea346f235
hyukjukim <hyukjukim@koreanair.com>
2016-12-28 19:21:44 +0900

    20151228

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const actions = {
     }
   },
   // You should implement your custom actions here
-  getForecast({context}) {
+  getForecast(context) {
 
     console.log('4');
     console.log('entities');
