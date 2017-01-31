4b7fd9299932ba50dfea416b0461e0e7cdc4a6ed
hyukjukim <hyukjukim@koreanair.com>
2017-01-31 16:50:38 +0900

    20170131

--- modules/Hero.js
+++ modules/Hero.js
@@ -1,7 +1,26 @@
 // ./modules/Hero.js
 
+
+
 function creatHero(){
-  console.log("들어왔냐");
+        res.send({
+          "message": {
+            "text": "원하는 캐릭터를 선택해 주세요",
+          },
+          "keyboard": {
+            "type": "buttons",
+            "buttons": [
+              "뚜벅이전사",
+              "간지러운궁수",
+              "몸빵약한법사",
+              "마스터"
+            ]
+          }
+        });
+            console.log(Math.floor(Math.random()*10));
+            console.log(Math.floor(Math.random()*10));
+            console.log(Math.floor(Math.random()*10));
+            console.log(Math.floor(Math.random()*10));
 }
 
 exports.creatHero = creatHero;
