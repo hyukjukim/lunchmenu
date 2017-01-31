71dad86bd0de0f9a63f8f1a9b5e8a3d30bc6c6b5
hyukjukim <hyukjukim@koreanair.com>
2017-01-31 14:01:29 +0900

    20170131

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var app = express();
 var name_flag_array = new Array("");
 var name_array = new Array("");
 var kakaousers = '';
-var Kakaomsg = require("../models/Kakaomsg");
+var Kakaomsg = require("./models/Kakaomsg");
 
 //DB Setting : 환경 변수를 사용하여 MONGO_DB에 접속합니다.
 mongoose.connect(process.env.MONGO_DB);
