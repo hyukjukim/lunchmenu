31af035c5dc4d845eb789d284f54802085f2633c
hyukjukim <hyukjukim@koreanair.com>
2017-01-31 10:52:19 +0900

    20170131

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ app.post('/message', function(req, res) {
           new: true
       }, function(error, doc) {
         if (err) return res.json(err);
-        obj = JSON.stringify(users); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
+        obj = JSON.stringify(doc); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
         kakaousers = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
       });
 
