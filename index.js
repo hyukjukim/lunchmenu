01d714e5b63e3a336e0b3d68e5b55c2d0b922cf6
hyukjukim <hyukjukim@koreanair.com>
2017-01-31 15:32:52 +0900

    20170131

--- index.js
+++ index.js
@@ -185,17 +185,10 @@ app.post('/message', function(req, res) {
 
         if (req.body.content === '전투시작') {
           res.send({
-  "text": "안녕하세요.",
-  "photo": {
-    "url": "https://hello.photo.src",
-    "width": 640,
-    "height": 480
-  },
-  "message_button": {
-    "label": "반갑습니다.",
-    "url": "http://hello.world.com/example"
-  }
-});
+              "message": {
+                  "text": "아니 시발"
+              }
+          });
       }
 
 });
