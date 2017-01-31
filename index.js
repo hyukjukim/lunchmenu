3a18213eab66baf0020d41248f293b00842c5ce8
hyukjukim <hyukjukim@koreanair.com>
2017-01-31 16:42:24 +0900

    20170131

--- index.js
+++ index.js
@@ -177,20 +177,6 @@ app.post('/message', function(req, res) {
     if (req.body.content === '캐릭터생성') {
       hero.creatHero();
 
-      res.send({
-        "message": {
-          "text": "원하는 캐릭터를 선택해 주세요",
-        },
-        "keyboard": {
-          "type": "buttons",
-          "buttons": [
-            "뚜벅이전사",
-            "간지러운궁수",
-            "몸빵약한법사",
-            "마스터"
-          ]
-        }
-      });
           console.log(Math.floor(Math.random()*10));
           console.log(Math.floor(Math.random()*10));
           console.log(Math.floor(Math.random()*10));
