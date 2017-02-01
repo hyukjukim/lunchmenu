ad9180df362bf7a04bfe48febd7888a8f3a33c2d
hyukjukim <hyukjukim@koreanair.com>
2017-02-01 20:38:53 +0900

    20170201

--- index.js
+++ index.js
@@ -372,9 +372,6 @@ app.post('/message', function(req, res) {
         }, {
             new: true
         }, function(err, users) {
-            if (err) {
-                console.log("Something wrong when updating data!");
-            }
             obj = JSON.stringify(users); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
             kakaousers = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
         });
