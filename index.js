c2a08d3440f7e35c78b048f750c83f5d43b2eb45
kimhyukju <kimhyukju@hanmail.net>
2017-02-09 23:40:23 +0900

    20170209

--- index.js
+++ index.js
@@ -266,7 +266,7 @@ console.log('9');
                   menus = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
                           res.send({
                             "message": {
-                              "text": "["+req.body.content,+"]의 메뉴는\n"+menus.menu+"\n\n입니다."},
+                              "text": "["+req.body.content+"]의 메뉴는\n"+menus.menu+"\n\n입니다."},
                             "keyboard": {
                               "type": "buttons",
                               "buttons": ["월요일","화요일","수요일 ☞☞☞","목요일","금요일","처음으로"]
