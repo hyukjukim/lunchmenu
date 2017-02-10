5ad697584512f8c9b408911280cbdee117b1fcf2
hyukjukim <hyukjukim@koreanair.com>
2017-02-10 14:26:17 +0900

    20170210

--- index.js
+++ index.js
@@ -269,9 +269,8 @@ console.log('9');
 
 
     else if (req.body.content === "한식"||req.body.content === "양식/일품"){
-      console.log(d.getDate());
-      console.log(d.getMonth()+1);
-      console.log(d.getFullYear());
+      
+        console.log(d.getFullYear()+'0'+d.getMonth()+1+d.getDate());
           //findOne
               menu.findOne({
                   'week': '월요일',
