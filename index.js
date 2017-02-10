ae716ca46c06c578dc41f5d7ef918c8c8757e48c
hyukjukim <hyukjukim@koreanair.com>
2017-02-10 15:34:54 +0900

    20170210

--- index.js
+++ index.js
@@ -284,7 +284,7 @@ console.log('9');
                   menus = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
                           res.send({
                             "message": {
-                              "text": "오늘의 ["+req.body.content+"] 메뉴는\n\n"+menus.menu1+"\n"+menus.menu2+"\n"+menus.menu3+"\n"+menus.menu4+"\n"+menus.menu5+"\n"+menus.menu6+"\n"+menus.menu7+"입니다."},
+                              "text": "오늘의 ["+req.body.content+"] 메뉴는\n\n"+menus.menu1+"\n"+menus.menu2+"\n"+menus.menu3+"\n"+menus.menu4+"\n"+menus.menu5+"\n"+menus.menu6+"\n"+menus.menu7+"\n입니다."},
                             "keyboard": {
                               "type": "buttons",
                               "buttons": ["처음으로","메뉴 점수 주기"]
