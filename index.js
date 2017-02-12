13aceb4030cd355986fa4fa9218a6cdc08c86f87
kimhyukju <kimhyukju@hanmail.net>
2017-02-12 21:48:32 +0900

    20170212

--- index.js
+++ index.js
@@ -171,7 +171,7 @@ keuser.findOne({
                               console.log(menus.menu1);
                               res.send({
                                 "message": {
-                                  "text": menus[1]
+                                  "text": obj[1]
                                 },
                                 "keyboard": {
                                   "type": "buttons",
