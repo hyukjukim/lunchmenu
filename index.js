5d1117e617481ddb0143cacf156ee2d3106ef581
hyukjukim <hyukjukim@koreanair.com>
2017-02-13 18:31:35 +0900

    20170213

--- index.js
+++ index.js
@@ -150,7 +150,6 @@ keuser.findOne({
               }
 
               else if (req.body.content === '전체 식단보기'){
-
                 //findOneAndUpdate
                           menu.find({
                               'date': '13'
@@ -158,11 +157,6 @@ keuser.findOne({
                               if (err) {
                                   console.log("Something wrong when updating data!");
                               }
-                              //obj = JSON.stringify(menus); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
-                              //menus = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
-                              console.log(obj);
-                              console.log(menus[1]);
-                              console.log(menus);
                               res.send({
                                 "message": {
                                   "text": menus.toString()
