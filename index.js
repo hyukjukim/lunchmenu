01ad846449263ba07c32aefbf85f26a5352489f1
hyukjukim <hyukjukim@koreanair.com>
2017-02-13 18:07:26 +0900

    20170213

--- index.js
+++ index.js
@@ -167,8 +167,8 @@ keuser.findOne({
                               if (err) {
                                   console.log("Something wrong when updating data!");
                               }
-                              obj = JSON.stringify(menus); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
-                              menus = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
+                              //obj = JSON.stringify(menus); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
+                              //menus = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
                               //console.log(obj);
                               //console.log(menus[1]);
                               //console.log(menus);
