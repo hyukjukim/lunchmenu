83fb742a658ff12d6eaab93e00ef7b1422b054dc
hyukjukim <hyukjukim@koreanair.com>
2017-02-13 18:02:41 +0900

    20170213

--- index.js
+++ index.js
@@ -167,12 +167,12 @@ keuser.findOne({
                               }
                               obj = JSON.stringify(menus); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
                               menus = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
-                              //console.log(obj);
-                              //console.log(menus[1]);
-                              //console.log(menus);
+                              console.log(obj);
+                              console.log(menus[1]);
+                              console.log(menus);
                               res.send({
                                 "message": {
-                                  "text": menus.menu1
+                                  "text": menus.menu1+ "dlqslek"
                                 },
                                 "keyboard": {
                                   "type": "buttons",
