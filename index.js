6f2606e3c9425d9e3810aa6509730453cea81e23
hyukjukim <hyukjukim@koreanair.com>
2017-02-13 18:00:01 +0900

    20170213

--- index.js
+++ index.js
@@ -165,14 +165,14 @@ keuser.findOne({
                               if (err) {
                                   console.log("Something wrong when updating data!");
                               }
-                              obj = JSON.stringify(menus); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
-                              menus = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
-                              console.log(obj);
-                              console.log(menus[1]);
-                              console.log(menus);
+                              //obj = JSON.stringify(menus); //객체 또는 배열을 인자로 받아 string을 json 형식으로 변경
+                            //  menus = JSON.parse(obj); //json 파싱하기 위해 변수에 배정
+                              //console.log(obj);
+                              //console.log(menus[1]);
+                              //console.log(menus);
                               res.send({
                                 "message": {
-                                  "text": menus.toString()
+                                  "text": menus.menu1
                                 },
                                 "keyboard": {
                                   "type": "buttons",
